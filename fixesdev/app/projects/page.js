75023098151dbc07f07bf052743eedf9b54f2c7d
rafaelvf <reviterif@gmail.com>
2024-01-28 22:15:00 -0500

    avances

--- fixesdev/app/projects/page.js
+++ fixesdev/app/projects/page.js
@@ -1,5 +1,68 @@
 import React from "react";
-
+import Image from "next/image";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 export default function Projects() {
-  return <div className="p-4">Projects</div>;
+  return (
+    <div className="col-span-10 flex p-4">
+      <Sheet>
+        <Card className=" h-fit max-w-[350px] relative pb-4">
+          <CardHeader>
+            <CardTitle>Hazmeunaoferta</CardTitle>
+          </CardHeader>
+          <Image
+            alt="Hazmeunaoferta"
+            width={200}
+            height={200}
+            src="/images/marriott1.webp"
+            className="mx-auto "
+          />
+          <p className="text-[14px] font-medium max-w-[320px] mx-auto p-6">
+            Is an e-commerce platform for AlmacenesMarriott, a prominent
+            retailer specializing in lighting and furniture. This development
+            introduces a unique twist where clients have the opportunity to
+            negotiate the prices of products.
+          </p>
+          <div className="flex justify-evenly px-6">
+            <Button>Website</Button>
+            <SheetTrigger asChild>
+              <Button variant="outline">See more</Button>
+            </SheetTrigger>
+          </div>
+        </Card>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Hazmeunaoferta</SheetTitle>
+            <SheetDescription>
+              HazmeUnaOferta is an e-commerce platform for AlmacenesMarriott, a
+              prominent retailer specializing in lighting and furniture. This
+              development introduces a unique twist where clients have the
+              opportunity to negotiate the prices of products.
+            </SheetDescription>
+            <SheetDescription>
+              The platform is built using advanced technologies such as Next.js,
+              React, TypeScript, SCSS, Sanity, and integrates with the Paymetez
+              payment processor, as well as MongoDB for database management.
+            </SheetDescription>
+          </SheetHeader>
+
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Save changes</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
 }
--- fixesdev/components/Sidebar.jsx
+++ fixesdev/components/Sidebar.jsx
@@ -2,17 +2,21 @@
 import React from "react";
 import { sidebar } from "../data/sidebar";
 import { useRouter } from "next/navigation";
-import { useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 export default function Sidebar() {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  console.log(sidebar, "sidebar");
+  const pathname = usePathname();
+  const parts = pathname?.split("/");
+  const lastPart = parts[parts.length - 1];
+
   return (
     <div className="col-span-2 border-r border-[#ededed] p-6 flex flex-col gap-6 bg-[#ffffff]">
       {sidebar.map((i, index) => (
         <p
           key={index}
-          className="cursor-pointer hover:bg-[#f5f5f5] p-2 rounded"
+          className={`cursor-pointer hover:bg-[#f5f5f5] p-2 rounded ${
+            i.coincidencia === lastPart ? "bg-[#f5f5f5]" : "null"
+          }`}
           onClick={() => {
             if (i.ruta.startsWith("http://") || i.ruta.startsWith("https://")) {
               window.open(i.ruta, "_blank");
