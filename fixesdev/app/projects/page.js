005626524ded6e67c47e49839990528470691523
rafaelvf <reviterif@gmail.com>
2024-01-31 17:09:43 -0500

    as

--- fixesdev/app/projects/page.js
+++ fixesdev/app/projects/page.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -12,71 +13,97 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import projectsData from "@/data/projects";
 export default function Projects() {
-  return (
-    <div className="col-span-12 md:col-span-10 flex p-4">
-      <Sheet>
-        <Card className=" h-fit max-w-[350px] relative pb-4">
-          <CardHeader>
-            <CardTitle>Hazmeunaoferta</CardTitle>
-          </CardHeader>
-          <Image
-            alt="Hazmeunaoferta"
-            width={200}
-            height={200}
-            src="/images/marriott1.webp"
-            className="mx-auto "
-          />
-          <p className="text-[14px] font-medium max-w-[320px] mx-auto p-6">
-            Is an e-commerce platform for AlmacenesMarriott, a prominent
-            retailer specializing in lighting and furniture. This development
-            introduces a unique twist where clients have the opportunity to
-            negotiate the prices of products.
-          </p>
-          <div className="flex justify-evenly px-6">
-            <Button>Website</Button>
-            <SheetTrigger asChild>
-              <Button variant="outline">See more</Button>
-            </SheetTrigger>
-          </div>
-        </Card>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Hazmeunaoferta</SheetTitle>
-            <SheetDescription>
-              HazmeUnaOferta is an e-commerce platform for AlmacenesMarriott, a
-              prominent retailer specializing in lighting and furniture. This
-              development introduces a unique twist where clients have the
-              opportunity to negotiate the prices of products.
-            </SheetDescription>
-            <SheetDescription>
-              The platform is built using advanced technologies such as Next.js,
-              React, TypeScript, SCSS, Sanity, and integrates with the Paymetez
-              payment processor, as well as MongoDB for database management.
-            </SheetDescription>
-          </SheetHeader>
-          <div className="flex flex-col gap-2 mt-2">
-            <p className="font-semibold ">Key points</p>
-            <ul className="list-disc text-[14px] text-[#647488] pl-4">
-              <li>Is integrated with their SAP system</li>
-              <li>Integrates Paymetez which is a payment processor</li>
-              <li>
-                Uses Sanity as a CMS to upload the images for their products
-              </li>
-              <li>
-                Uses Sanity as a CMS to upload the images for their products
-              </li>
-            </ul>
-          </div>
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
 
-          <SheetFooter className="mt-4">
-            <SheetClose className="flex gap-4">
-              <Button>Website</Button>
-              <Button variant="outline">Video</Button>
-            </SheetClose>
-          </SheetFooter>
-        </SheetContent>
-      </Sheet>
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+          },
+          (error) => {
+            console.error("Error getting location:", error);
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+    };
+
+    getLocation(); // Call getLocation when component mounts
+  }, []);
+  console.log(latitude, longitude, "lati");
+  return (
+    <div className="col-span-12 md:col-span-10 flex gap-4 flex-wrap p-4 justify-center md:justify-start">
+      {projectsData.map((project, index) => (
+        <Sheet>
+          <Card className=" h-fit max-w-[350px] relative pb-4" key={index}>
+            <CardHeader>
+              <CardTitle>{project.title}</CardTitle>
+            </CardHeader>
+            <div className="h-[200px] flex items-center">
+              <Image
+                alt="Hazmeunaoferta"
+                width={200}
+                height={200}
+                src={project.image}
+                className="m-auto "
+              />
+            </div>
+            <p className="text-[14px] font-medium max-w-[320px] mx-auto p-6">
+              {project.shortDescription}
+            </p>
+            <div className="flex justify-evenly px-6">
+              <a href={project.url} target="_blank" rel="noopener noreferrerz">
+                <Button>Website</Button>
+              </a>
+              <SheetTrigger asChild>
+                <Button variant="outline">See more</Button>
+              </SheetTrigger>
+            </div>
+          </Card>
+
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>{project.title}</SheetTitle>
+              <SheetDescription>{project.shortDescription}</SheetDescription>
+              <SheetDescription>{project.longDescription}</SheetDescription>
+            </SheetHeader>
+            <div className="flex flex-col gap-2 mt-2">
+              <p className="font-semibold ">Key points</p>
+              <ul className="list-disc text-[14px] text-[#647488] pl-4">
+                {project.keyPoints.map((point, innerIndex) => (
+                  <li key={innerIndex}>{point}</li>
+                ))}
+              </ul>
+            </div>
+
+            <SheetFooter className="mt-4">
+              <SheetClose className="flex gap-4">
+                <a
+                  href={project.url}
+                  target="_blank"
+                  rel="noopener noreferrerz"
+                >
+                  <Button>Website</Button>
+                </a>
+                <a
+                  href={project.video}
+                  target="_blank"
+                  rel="noopener noreferrerz"
+                >
+                  <Button variant="outline">Video</Button>
+                </a>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      ))}
     </div>
   );
 }
---
+++ fixesdev/data/projects.js
@@ -0,0 +1,57 @@
+const projectsData = [
+  {
+    title: "Cuquipay",
+    shortDescription:
+      "Cuquipay is a platform specialized in accelerating and facilitating market entry for entrepreneurs or small businesses. We aim to democratize e-commerce and make it possible for all businesses to sell online.",
+    longDescription:
+      "Cuquipay is a powerful tool designed to streamline the creation of your e-commerce platform. In just 10 minutes, you can effortlessly share your product catalog across your social media channels, enabling seamless order processing. With Cuquipay, you gain access to a comprehensive platform for managing your products, tracking orders, creating promotions, and much more.",
+    keyPoints: [
+      "Clients receive a fully operational e-commerce website, ready to showcase and sell their products effectively.",
+      "Easily view and manage orders, ensuring smooth and accurate processing",
+      "Gain valuable insights into your business performance with detailed analytics data",
+      "Create enticing offers for your customers with the ability to generate custom coupons.",
+      " Drive sales and customer engagement by creating attractive product combos and promotions.",
+      "Customize your work schedules to align with your business needs, enhancing flexibility and efficiency",
+      "Cuquipay is crafted using cutting-edge technologies, including Next.js, Node.js, JavaScript, TypeScript, Cloudinary, MongoDB, and Tailwind CSS. ",
+    ],
+    url: "https://www.cuquipay.com/",
+    video: "https://youtu.be/3qfj11HvtRg",
+    image: "/images/cuqipay.png",
+  },
+  {
+    title: "Hallados",
+    shortDescription:
+      "Is an e-commerce platform for AlmacenesMarriott, a prominent retailer specializing in lighting and furniture. This development introduces a unique twist where clients have the opportunity to negotiate the prices of products.",
+    longDescription:
+      " The platform is built using advanced technologies such as Next.js, React, TypeScript, SCSS, Sanity, and integrates with the Paymetez payment processor, as well as MongoDB for database management.",
+    keyPoints: [
+      "Bot that deals price with customers",
+      "Is integrated with their SAP system",
+      "Integrates Paymetez payment processor that accepts Visa, Mastercard, Amex, etc",
+      "Uses Sanity as a CMS to upload the images for their products",
+      "Backoffice for admins so they can manage their sales and products",
+    ],
+    url: "https://hazmeunaoferta.almacenesmarriott.com/",
+    video: "https://youtu.be/FPHc5nTIusM",
+    image: "/images/hallados.png",
+  },
+  {
+    title: "HazmeUnaOferta",
+    shortDescription:
+      "Is an e-commerce platform for AlmacenesMarriott, a prominent retailer specializing in lighting and furniture. This development introduces a unique twist where clients have the opportunity to negotiate the prices of products.",
+    longDescription:
+      " The platform is built using advanced technologies such as Next.js, React, TypeScript, SCSS, Sanity, and integrates with the Paymetez payment processor, as well as MongoDB for database management.",
+    keyPoints: [
+      "Bot that deals price with customers",
+      "Is integrated with their SAP system",
+      "Integrates Paymetez payment processor that accepts Visa, Mastercard, Amex, etc",
+      "Uses Sanity as a CMS to upload the images for their products",
+      "Backoffice for admins so they can manage their sales and products",
+    ],
+    url: "https://hazmeunaoferta.almacenesmarriott.com/",
+    video: "https://youtu.be/FPHc5nTIusM",
+    image: "/images/marriott1.webp",
+  },
+];
+
+export default projectsData;
