72dc31c390fdbb02f28f2e0e9e834a5810c2b9db
rafaelvf <reviterif@gmail.com>
2024-02-05 09:49:05 -0500

    d

--- fixesdev/app/layout.js
+++ fixesdev/app/layout.js
@@ -4,7 +4,8 @@ import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import Sidebarmobile from "@/components/Sidebarmobile";
 const inter = Inter({ subsets: ["latin"] });
-
+import Image from "next/image";
+import Link from "next/link";
 export const metadata = {
   title: "Rafael Viteri",
   description: "Rafael Viteri (fixesdev) portfolio",
@@ -16,9 +17,17 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {" "}
         <Navbar />
-        <div className="grid grid-cols-12 bg-[#fafafa] h-full">
+        <div className="relative grid grid-cols-12 bg-[#fafafa] h-full">
           <Sidebar />
           <Sidebarmobile />
+          <Link
+            href="https://wa.me/593994257528"
+            target="_blank"
+            rel="noopener noreferrerz"
+            className="fixed hidden md:flex md:bottom-4 md:right-4 hover:scale-110 transform z-10"
+          >
+            <Image src="/icons/was.svg" alt="contact" width={42} height={42} />
+          </Link>
           {children}
         </div>
       </body>
